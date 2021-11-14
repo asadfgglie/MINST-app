519ba47e1245dedfc9a2c99d356c4a8b693c46b7
asadfgglie <ac121878@outlook.com>
2021-11-14 23:58:35 +0800

    Vsersion 1.3

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 chcp 65001
-pip install -r requirements.txt 
+pip install -r requirements.txt
 pyinstaller ^
     -D writter.py ^
     -w ^
@@ -10,3 +10,11 @@ pyinstaller ^
     --specpath %cd%\build ^
     -n MNIST手寫辨識 ^
     --clean
+
+if  %errorlevel% == 0 (
+    echo 編譯完成!
+    echo 檔案位於 %cd%\dist\MNIST手寫辨識\MNIST手寫辨識.exe
+)  else  (
+    echo 編譯失敗!
+    echo 錯誤代碼: %errorlevel%
+)
